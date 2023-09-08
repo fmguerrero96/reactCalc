81c0b169989ee25725613708597906402ebd0fbd
Misael Guerrero <mguerrerowest@gmail.com>
2023-09-08 11:25:54 -0400

    Place screens and number buttons

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,19 @@ import Screen from "./components/Screen"
 
 export default function App() {
   return (
-    <div>
-      
+    <div className="mainContainer">
+      <Screen position={'upper'}/>
+      <Screen position={'lower'}/>
+      <NumberButton number={1}/>
+      <NumberButton number={2}/>
+      <NumberButton number={3}/>
+      <NumberButton number={4}/>
+      <NumberButton number={5}/>
+      <NumberButton number={6}/>
+      <NumberButton number={7}/>
+      <NumberButton number={8}/>
+      <NumberButton number={9}/>
+      <NumberButton number={0}/>
     </div>
   )
 }
